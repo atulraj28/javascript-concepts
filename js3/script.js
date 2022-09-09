21f9507d51fd6c268aafb6ad5e730b3e2a8f5a0c
Atul raj <atulraj.ar123@gmail.com>
2022-09-09 14:14:13 +0530

    added next day file

---
+++ js3/script.js
@@ -0,0 +1,16 @@
+/*var digit1 = 10;
+var x =2;
+var z=digit1+x;
+
+alert(digit1);*/
+
+/*let digit1 = 10;
+let x =2;
+let z =digit1+x;*/
+
+let digit1=10;
+const x=2;
+digit1=20;
+console.log(digit1);
+
+let a=5,y="john";
